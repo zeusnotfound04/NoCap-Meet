efb7f827e277ca945e58c8f18f12dddb799135c1
zeusnotfound04 <zeusnotfound0@gmail.com>
2025-07-19 03:11:47 +0530

    feat: made the peer types

---
+++ src/types/peer.ts
@@ -0,0 +1,16 @@
+import Peer, { DataConnection, MediaConnection } from "peerjs";
+
+export interface PeerConnection {
+    peer : Peer;
+    mediaConnection : Map<string, MediaConnection>;
+    dataConnection : Map<string, DataConnection>
+    localStream? : MediaConnection ;
+    screenStream :MediaConnection
+}
+
+export interface PeerData {
+    type : 'chat' | 'system' | 'media-control' | 'user-info'
+    payload : any ;
+    timeStamp : Date ;
+    senderId: string
+}
